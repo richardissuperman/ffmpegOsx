60445fac57c90a4eb9912b4e21e41640f4edc2b2
ZhongQing <richard_johnson@sina.com>
2017-06-16 17:57:47 +0800

    Initial Commit

---
+++ TestFFMPEG/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestFFMPEG
+//
+//  Created by ZhongQing on 16/06/17.
+//  Copyright © 2017年 ZhongQing. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
