d88910017bd03e3e057c7cd491beff24d852eb89
ZhongQing <richard_johnson@sina.com>
2017-06-19 20:23:12 +0800

    update the OSX ffmpeg sample

--- TestFFMPEG/main.cpp
+++ TestFFMPEG/main.cpp
@@ -7,9 +7,18 @@
 //
 
 #include <iostream>
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libavutil/imgutils.h>
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    printf("%s",avcodec_configuration());
+    avcodec_register_all();
+    avformat_network_init();
     return 0;
 }
