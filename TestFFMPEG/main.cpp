61e6c21f4ff98aa6d4affbf151503d16f32124fb
ZhongQing <richard_johnson@sina.com>
2017-07-22 20:40:35 +0800

    update code to init the av format context

--- TestFFMPEG/main.cpp
+++ TestFFMPEG/main.cpp
@@ -20,5 +20,23 @@ int main(int argc, const char * argv[]) {
     printf("%s",avcodec_configuration());
     avcodec_register_all();
     avformat_network_init();
+    av_register_all();
+    
+    AVFormatContext	*pFormatCtx;
+    pFormatCtx = avformat_alloc_context();
+
+    
+    const char  *filenameSrc = "/Users/zhongqing/Documents/Singing/Mo.mp4";
+    
+    int result  = avformat_open_input(&pFormatCtx,filenameSrc,NULL,NULL)!=0;
+    
+    
+    if(result<0){
+        printf("\n Couldn't open input stream.   error code %d \n",result);
+        return -1;
+    }
+    
+    printf("\n Time is %d \n",pFormatCtx->duration);
+    
     return 0;
 }
